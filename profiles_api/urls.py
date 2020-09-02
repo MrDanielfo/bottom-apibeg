312c621d6c381ba3570fdaedfb01b1659d10db3b
Josué Daniel Flores <mr.danielfo@gmail.com>
2020-09-02 17:45:06 -0500

    ViewSet Added

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+# es necesario el include para la lista de urls del viewset
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
 urlpatterns = [
      path('hello-view/', views.HelloApiView.as_view()),
+     # ver la propiedad urls que generan las viewset
+     path('', include(router.urls))
 ]
 
